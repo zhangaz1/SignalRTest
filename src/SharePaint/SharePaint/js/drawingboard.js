53bfd2b62df0ccc88c8d06741fa60b3a4329dbbc
zhangaz1 <zhangaz1@hotmail.com>
2016-08-08 18:12:23 +0800

    可用的示例

--- src/SharePaint/SharePaint/js/drawingboard.js
+++ src/SharePaint/SharePaint/js/drawingboard.js
@@ -37,4 +37,54 @@
     $('#clear').click(clearPoints);
 
 
+    var hub = $.connection.drawingBoard;
+    hub.state.color = colorElement.val();
+    var connected = false;
+
+    colorElement.change(function () {
+        hub.state.color = $(this).val();
+    });
+
+
+    canvas.mousemove(function (e) {
+        if (buttonPressed && connected) {
+            hub.server.broadcastPoint(
+                Math.round(e.offsetX),
+                Math.round(e.offsetY)
+            );
+        }
+    });
+
+    $('#clear').click(function () {
+        if (connected) {
+            hub.server.broadcastClear();
+        }
+    });
+
+    hub.client.clear = clearPoints;
+
+    hub.client.drawPoint = function (x, y, color) {
+        setPoint(x, y, color);
+    }
+
+    hub.client.update = function (points) {
+        if (!points) {
+            return;
+        }
+
+        for (var x = 0; x < points.length; x++) {
+            var row = points[x];
+            for (y = 0; y < row.length; y++) {
+                var color = row[y];
+                if (color) {
+                    setPoint(x, y, color);
+                }
+            }
+        }
+    }
+
+    $.connection.hub.start()
+        .done(function () {
+            connected = true;
+        });
 });
